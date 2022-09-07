a447bfdd0976658fe19a6da779a435c260aaa561
Jared Grippe <jared@deadlyicon.com>
2022-09-06 17:06:40 -0700

    added defaultToFirst to IdentifierSelectInput

--- client/src/components/IdentifierSelectInput.js
+++ client/src/components/IdentifierSelectInput.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useRef, useEffect } from 'react'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 import FormControl from '@mui/material/FormControl'
@@ -11,24 +12,35 @@ import { useMyIdentifiers } from '../resources/identifiers'
 import ErrorMessage from './ErrorMessage'
 
 export default function IdentifierSelectInput({
-  value, onChange, disabled = false, ...props
+  value, onChange, disabled = false, defaultToFirst = false, ...props
 }){
+  const selectRef = useRef()
   const [identifiers, identifiersReq] = useMyIdentifiers()
   const [profiles, profilesReq] = useMyProfiles()
 
   const loading = identifiersReq.loading || profilesReq.loading
   const error = identifiersReq.error || profilesReq.error
 
+  const firstIdentifier = identifiers && identifiers[0]
+  const shouldSetToFirst = (defaultToFirst && firstIdentifier && !value)
+  useEffect(
+    () => {
+      if (shouldSetToFirst) onChange(firstIdentifier.id)
+    },
+    [shouldSetToFirst]
+  )
+
   return <FormControl fullWidth>
     <InputLabel id="IdentifierSelectInputLabel">Identifier</InputLabel>
     <ErrorMessage error={error}/>
     <Select {...{
+      ref: selectRef,
       name: 'identity',
       ...props,
       labelId: 'IdentifierSelectInputLabel',
       disabled: !!(disabled || loading),
       value,
-      onChange,
+      onChange(event){ onChange(event.target.value) },
     }}>
       {loading ? null : identifiers.map(identifier => {
         const profile = profiles
