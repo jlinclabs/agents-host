60c9c5d37d4fdcda65f84e53e72658b022ebe443
Jared Grippe <jared@deadlyicon.com>
2022-09-13 17:14:10 -0700

    WIP

--- server/jlinxRestApi.js
+++ server/jlinxRestApi.js
@@ -1,4 +1,5 @@
 import Router from 'express-promise-router'
+import prisma from '../prisma/client.js'
 import Agent from './Agent/index.js'
 import { JlinxClient } from './jlinx.js'
 
@@ -14,9 +15,28 @@ router.post('/agreements/signatures', async (req, res) => {
   const did = agreement.metadata.controllers[0]
 
   // ack signature
-  const agent = await Agent.find(did)
+  const agent = await openAgent(did)
   await agent.agreements.ackSignature(signatureId)
   res.json({ success: true })
 })
 
 export default router
+
+
+async function openAgent(did){
+  const record = await prisma.agent.findUnique({
+    where: { did },
+    select: {
+      didSecret: true,
+      vaultKey: true,
+    }
+  })
+  if (!record) {
+    throw new Error(`agent not hosted here did=${did}`)
+  }
+  return await Agent.open({
+    did,
+    didSecret: Buffer.from(record.didSecret, 'hex'),
+    vaultKey: record.vaultKey,
+  })
+}
