4c2660dfcd34ce86d9adb74701175fbe323f1bf2
Jared Grippe <jared@deadlyicon.com>
2022-10-17 11:53:28 -0700

    WIP

--- server/queries/auth.js
+++ server/queries/auth.js
@@ -2,6 +2,6 @@ export * from 'app-shared/server/queries/auth.js'
 
 export async function getCurrentUser({}, context){
   return await context.queries.auth._selectCurrentUser({
-    agents: true,
+    did: true,
   })
 }
