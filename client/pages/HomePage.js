7cef51fb818740e3580421a1b8c6af4bfb6d12cc
Jared Grippe <jared@deadlyicon.com>
2022-10-25 11:11:32 -0700

    WIP

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -1,13 +1,60 @@
-import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
+import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import SupportAgentIcon from '@mui/icons-material/SupportAgent'
+import backgroundVideo from 'raw:../media/pexels-ehab-el-gapry-6238188.mp4'
 
-import LinkToDid from '../components/LinkToDid'
-import CopyButton from '../components/CopyButton'
+console.log('backgroundVideo', backgroundVideo)
 
 export default function HomePage() {
-  return <Container maxWidth="md">
-    <Typography>Agents!</Typography>
-    <Typography>brought to you by jlinc labs</Typography>
-  </Container>
+  return <Box sx={{
+    position: 'relative',
+    with: '100vw',
+    minHeight: '100vh',
+  }}>
+    <Box component="video" autoPlay muted loop sx={{
+      width: '100vw',
+      height: '100vh',
+      objectFit: 'cover',
+      position: 'fixed',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      zIndex: -1,
+    }}>
+      <source src={backgroundVideo} type="video/mp4"/>
+    </Box>
+    <Stack
+      direction={{ xs: 'column', sm: 'row' }}
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        width: '100vw',
+        minHeight: '100vh',
+      }}
+    >
+      <SupportAgentIcon sx={{fontSize: 300}}/>
+      <Box sx={{
+        textAlign: { xs: 'center', sm: 'left' },
+        p: 2
+      }}>
+        <Typography
+          variant="h3"
+          component="h1"
+        >Agents!</Typography>
+        <Typography
+          variant="h6"
+          component="h2"
+          sx={{my: 2}}
+        >Brought to you by Jlinc Labs</Typography>
+        <Button {...{
+          variant: 'contained',
+        }}>Activate Your Agent</Button>
+      </Box>
+    </Stack>
+  </Box>
 }
--- client/theme.js
+++ client/theme.js
@@ -8,10 +8,18 @@ export default createTheme({
     mode: 'dark',
     primary: {
       // main: colors.purple[600],
-      main: 'rgb(66, 165, 245)',
+      // main: 'rgb(66, 165, 245)',
+      // main: '#111119',
+      main: '#30a9bb',
     },
     secondary: {
       main: colors.blue[400],
+    },
+    typography: {
+      fontFamily: [
+        'proxima-nova',
+        'sans-serif',
+      ].join(',')
     },
   },
   components: {
