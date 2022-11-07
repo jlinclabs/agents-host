6e9e7497eb997319a6e903fdaff2404134e8d0f2
Jared Grippe <jared@deadlyicon.com>
2022-11-07 12:44:19 -0800

    WIP

--- server/queries/dids.js
+++ server/queries/dids.js
@@ -1,6 +1,7 @@
 export async function getDidDocument({ did }, context){
   const agent = await context.getAgent()
   console.log({ agent })
+  console.log({ did })
   const didDocument = await agent.resolveDID(did)
   console.log({ did, didDocument })
   return didDocument
--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,17 @@
 import uploads from 'app-shared/server/uploads.js'
-// import ceramicRestApi from './ceramicRestApi.js'
 import jlinxAgentApi from './jlinxAgentApi.js'
 import jlinxApp from './jlinxApp.js'
-// import { subscribeToNotificationsForUser } from './notifications.js'
 
 export default router => {
   router.get('/.well-known/did.json', async (req, res) => {
-    res.json(await jlinxApp.getDIDDocument())
+    const didDocument = await jlinxApp.getDIDDocument()
+    res.json(didDocument)
   })
+
   router.get('/agents/:publicKey/did.json', async (req, res) => {
     const { publicKey } = req.params
-    const didDocument = await jlinxApp.resolveDID(`did:key:${publicKey}`)
-    didDocument.id = `did:web:${req.origin}:agents:${publicKey}`
+    const { didDocument } = await jlinxApp.resolveDID(`did:key:${publicKey}`)
+    didDocument.id = `did:web:${req.host}:agents:${publicKey}`
     res.json(didDocument)
   })
 
@@ -20,70 +20,4 @@ export default router => {
     storagePath: process.env.UPLOADS_PATH
   }))
   router.use('/api/jlinx/v1', jlinxAgentApi)
-  // router.use('/api/ceramic', ceramicRestApi)
-
-  // router.get('/api/notifications', async (req, res) => {
-  //   const observable = await subscribeToNotificationsForUser(req.context.userId, req.context)
-  //   streamServerSentEvents(req, res, observable)
-  // })
 }
-
-//
-//
-// function streamServerSentEvents(req, res, observable){
-//   console.log('🚒 STARTING SSE STREAM!')
-//   let closed = false
-//   let subscription
-//   const close = () => {
-//     closed = true
-//     res.end()
-//     if (subscription) subscription.unsubscribe();
-//   }
-//
-//   req.on('end', () => {
-//     console.log('🚒 http request end')
-//   })
-//   req.on('close', () => {
-//     console.log('🚒 http request closed')
-//     close()
-//   })
-//   res.socket.on('close', function () {
-//     // res.on('close', () => {
-//     console.log('🚒 res.socket closed')
-//     close()
-//   })
-//
-//   res.set({
-//     'Cache-Control': 'no-cache',
-//     'Content-Type': 'text/event-stream',
-//     Connection: 'keep-alive'
-//   })
-//   res.flushHeaders()
-//   try{
-//     setInterval( // keep alice
-//       () => {
-//         res.write(`: keepalive\n\n`)
-//       },
-//       1000
-//     )
-//     subscription = observable.subscribe({
-//       next(event) {
-//         console.error('🚒 sse event', event)
-//         // console.log('CLOSED?', [res.socket])
-//         res.write(`data: ${JSON.stringify(event)}\n\n`)
-//       },
-//       error(error) {
-//         console.error('🚒 sse observer error', error)
-//         // console.log('CLOSED?', [res.socket])
-//         close()
-//       },
-//       complete() {
-//         console.log('🚒 observer completed')
-//         close()
-//       },
-//     })
-//   }catch (error) {
-//     console.error('🚒 ERRRRR', error)
-//     close()
-//   }
-// }
