79a87a88bd981d238f82ed745ca80df56cd123ff
Jared Grippe <jared@deadlyicon.com>
2023-03-07 17:18:47 -0800

    WIP

--- client/index.js
+++ client/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { render } from 'app-shared/client/render.js'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Routes from './Routes'
 import theme from './theme'
+import ErrorBoundary from './components/ErrorBoundary'
+import AppError from './components/AppError'
 
-render({
-  Routes,
-  theme,
-})
+const root = ReactDOM.createRoot(document.querySelector('body > main'))
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider {...{theme}}>
+      <CssBaseline enableColorScheme />
+      <ToastContainer/>
+      <ErrorBoundary onError={error => <AppError {...{error}}/>}>
+        <BrowserRouter>
+          <Routes/>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </ThemeProvider>
+  </React.StrictMode>
+)
